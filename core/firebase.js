447be18bca4021515df1bb6447f4099003494ee3
thierrybig74 <thierry.bigorne@gmail.com>
2026-04-03 12:56:06 +0200

    Update firebase.js
    Intégration api

--- core/firebase.js
+++ core/firebase.js
@@ -1,3 +1,19 @@
-// Firebase core - initialisation (placeholder)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
 
-export const firebaseReady = false;
+const firebaseConfig = {
+  apiKey: "AIzaSyDPTMRZfxbnv2Go8YzGqMLe_BwDCaNBKnA",
+  authDomain: "assistmat-suite.firebaseapp.com",
+  projectId: "assistmat-suite",
+  storageBucket: "assistmat-suite.firebasestorage.app",
+  messagingSenderId: "800585330619",
+  appId: "1:800585330619:web:9d79d7a6a54340b34ade9e"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
