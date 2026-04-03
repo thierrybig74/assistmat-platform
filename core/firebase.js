822b9bff852b3e8882aed0da0ac4e4ebfabf6e12
thierrybig74 <thierry.bigorne@gmail.com>
2026-04-03 13:19:52 +0200

    Update firebase.js
    update firebase core for firestore test

--- core/firebase.js
+++ core/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDPTMRZfxbnv2Go8YzGqMLe_BwDCaNBKnA",
@@ -16,4 +16,4 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { auth, db };
+export { auth, db, collection, addDoc };
